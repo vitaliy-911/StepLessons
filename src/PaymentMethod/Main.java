cfb5d373e980c02f9689a742a594aa6e06e7fce4
vitaliy-911 <nurnazarov.vitaliy@gmail.com>
2025-02-27 21:01:41 +0300

    Merge pull request #12 from vitaliy-911/home
    Home

--- src/PaymentMethod/Main.java
+++ src/PaymentMethod/Main.java
@@ -1,15 +1,21 @@
 package PaymentMethod;
 
-import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         PaymentMethod[] paymentMethods = new PaymentMethod[5];
         paymentMethods[0] = new Cash();
         paymentMethods[1] = new CreditCard(1234567, "Иванов Иван");
-        paymentMethods [2] = new PayPal("kozlov@ramler.ru");
+        paymentMethods[2] = new PayPal("kozlov@ramler.ru");
+        paymentMethods[3] = new CreditCard(1234567, "Иванов Иван");
 
 
+        CreditCard creditCard = new CreditCard(1234, "river");
+        CreditCard creditCard1 = new CreditCard(1234, "river");
+
+        System.out.println(creditCard1.equals(creditCard));
+        System.out.println(creditCard == creditCard1);
 
     }
 }
