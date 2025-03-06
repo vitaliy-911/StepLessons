d020b5237b6c8f1f38f9de4cf8b58670eada82ee
vitaliy-911 <nurnazarov.vitaliy@gmail.com>
2025-03-06 18:36:47 +0300

    wet

--- src/StudentWork/Main.java
+++ src/StudentWork/Main.java
@@ -11,5 +11,25 @@ public class Main {
         }
 
 
+    }  public static void findStudentName(List<Student> students, String name) {
+        for (Student student : students) {
+            if (student.getName().equals(name)) {
+                System.out.println(student);
+            }
+            System.out.println("Такого студента нет");
+            break;
+        }
+    }
+
+    public static void removeStudentName(List<Student> students, String name) {
+        students.removeIf(student -> student.getName().equals(name));
+    }
+
+    public static void addStudentToList(List<Student> students, List<Student> students1) {
+        students1.addAll(students);
+    }
+
+    public static void replaceStudentATIndex(List<Student> students, int index) {
+        students.set(index, StudentFactory.next());
     }
 }
--- src/StudentWork/Student.java
+++ src/StudentWork/Student.java
@@ -21,4 +21,8 @@ public class Student {
                 ", grade=" + grade +
                 '}';
     }
+
+    public String getName() {
+        return name;
+    }
 }
