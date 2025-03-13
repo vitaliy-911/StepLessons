fd5e6effe5eb349f31ae04fb9ed7c759f3f86f80
Vital <nurnazarov@rambler.ru>
2025-03-13 15:44:37 +0300

    vit

--- src/TransportWork2/Main.java
+++ src/TransportWork2/Main.java
@@ -1,8 +1,5 @@
 package TransportWork2;
 
-import java.util.List;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
         TransportManager manager = new TransportManager();
