56e7a4865aabad898b4a5559819987d12aa3197d
vitaliy-911 <nurnazarov.vitaliy@gmail.com>
2025-04-14 21:00:27 +0300

    Predicate

--- src/ClassWork/L23_01_25/Task00.java
+++
@@ -1,6 +0,0 @@
-package ClassWork.L23_01_25;
-
-public class Task00 {
-    public Task00() {
-    }
-}
--- src/ClassWork/Predicates/Task00.java
+++ src/ClassWork/Predicates/Task00.java
@@ -31,7 +31,8 @@ public class Task00 {
 
         //Лямда, параметры........тело
         filterByPredicate(apples, (Apple a) -> Color.RED.equals(a.getColor()));
-
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        filterByPredicate(numbers, (Integer i) -> i % 2==0);
 
     }
 
