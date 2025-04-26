05b6944adef8f5b1bd6f0130ecf1869bc5de116b
Vital <nurnazarov@rambler.ru>
2025-04-26 17:03:30 +0300

    Stream

--- src/HomeWork/GroceryItem/Main.java
+++ src/HomeWork/GroceryItem/Main.java
@@ -2,7 +2,7 @@ package HomeWork.GroceryItem;
 
 import HomeWork.StreamApi.Category;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -17,17 +17,61 @@ public class Main {
                 new Customer("Ирина", List.of(
                         new GroceryItem("Apple", Category.FRUIT, 0.5, true),
                         new GroceryItem("Wine", Category.BEVERAGE, 5.0, false),
-                        new GroceryItem("Cheese",Category.DAIRY, 3.0, true)
+                        new GroceryItem("Cheese", Category.DAIRY, 3.0, true)
                 )),
                 new Customer("Сергей", List.of(
                         new GroceryItem("Eggs", Category.DAIRY, 2.0, true),
                         new GroceryItem("Chocolate", Category.BAKERY, 1.5, false)
                 ))
         );
-       customers.stream()
-                .flatMap(l->l.getShoppingList()
+        List<GroceryItem> collect = customers.stream()
+                .flatMap(l -> l.getShoppingList()
+                        .stream())
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+        long count = customers.stream()
+                .map(Customer::getName)
+                .count();
+        System.out.println(count);
+
+        Optional<Customer> max = customers.stream()
+                .max(Comparator.comparingDouble(cus -> cus.getShoppingList()
                         .stream()
-                        .collect(Collectors.groupingBy()))
+                        .mapToDouble(GroceryItem::getPrice)
+                        .sum()));
+
+        max.ifPresent(customer -> System.out.println(customer));
+
+        Set<String> set = customers.stream()
+                .flatMap(customer -> customer.getShoppingList().stream())
+                .map(GroceryItem::getName)
+                .collect(Collectors.toSet());
+        System.out.println(set);
+
+        Map<Category, Set<String>> categorySetMap = customers.stream()
+                .flatMap(customer -> customer.getShoppingList()
+                        .stream()
+                        .map(item -> Map.entry(item.getCategory(), customer.getName())))
+                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue
+                        , Collectors.toSet())));
+        System.out.println(categorySetMap);
+
+        Optional<Customer> first = customers.stream()
+                .filter(customer -> customer.getShoppingList().isEmpty())
+                .findFirst();
+        System.out.println(first);
+
+        Map<String, DoubleSummaryStatistics> doubleSummaryStatisticsMap = customers.stream()
+                .collect(Collectors.toMap(Customer::getName, customer -> customer.getShoppingList()
+                        .stream().collect(Collectors.summarizingDouble(GroceryItem::getPrice))));
+        System.out.println(doubleSummaryStatisticsMap);
+
+         customers.stream()
+                .peek(customer -> System.out.println("обрабатываем" + customer.getName()));
+
+        
+
 
 
     }
